ae45a3976a4ffb4d00a88e3e87a10f13db5718a7
Manal Ziadeh <manalhz@gmail.com>
2022-05-16 12:46:10 -0400

    Using Redux

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import Main from './components/MainComponent';
 import React from 'react';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
 
 export default function App() {
   return (
-    <Main />
+    <Provider store={store}>
+      <Main />
+    </Provider>
   );
 }
