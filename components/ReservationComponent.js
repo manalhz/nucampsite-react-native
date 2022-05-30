5639e86268c12738263a679dea7b7e9fb0f38802
Manal Ziadeh <manalhz@gmail.com>
2022-05-30 19:55:45 -0400

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -11,6 +11,8 @@ import {
 } from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 import * as Animatable from "react-native-animatable";
+import * as Notifications from "expo-notifications";
+import { addNotificationsDroppedListener } from "expo-notifications";
 
 class Reservation extends Component {
   constructor(props) {
@@ -27,15 +29,6 @@ class Reservation extends Component {
   static navigationOptions = {
     title: "Reserve Campsite",
   };
-
-  resetForm() {
-    this.setState({
-      campers: 1,
-      hikeIn: false,
-      date: new Date(),
-      showCalendar: false,
-    });
-  }
 
   showAlert() {
     Alert.alert(
@@ -54,12 +47,50 @@ class Reservation extends Component {
         {
           text: "OK",
           onPress: () => {
+            this.presentLocalNotification(
+              this.state.date.toLocaleDateString("en-US")
+            );
             this.resetForm();
           },
         },
       ],
       { cancelable: true }
     );
+  }
+
+  resetForm() {
+    this.setState({
+      campers: 1,
+      hikeIn: false,
+      date: new Date(),
+      showCalendar: false,
+    });
+  }
+
+  async presentLocalNotification(date) {
+    function sendNotification() {
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true,
+        }),
+      });
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Your Campsite Reservation Search",
+          body: `Search for ${date} requested`,
+        },
+        trigger: null,
+      });
+    }
+
+    let permissions = await Notifications.getPermissionsAsync();
+    if (!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+    if (permissions.granted) {
+      sendNotification();
+    }
   }
 
   render() {
